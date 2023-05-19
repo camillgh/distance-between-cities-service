14a66770c02c34fdb61fc463081220b881c46a2b
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-05-19 15:18:21 +0200

    ✨ Add func that shows message onclick

--- distance-calculator.js
+++ distance-calculator.js
@@ -1,4 +1,16 @@
 
 function findDistance() {
-    alert("Testing")
+    let cityOne = document.querySelector("#cityOne");
+    let cityTwo = document.querySelector("#cityTwo");
+
+    let distance = 14;
+
+    displayDistance(distance);
+
+}
+
+function displayDistance(distance) {
+
+    let message = document.querySelector("#message");
+    message.innerHTML = "The distance is " + distance;    
 }
